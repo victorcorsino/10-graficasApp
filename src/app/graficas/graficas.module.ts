509e9a65da0688f15049ef249f128e7366fdd7cf
Victor Corsino <vcorsinoa@gmail.com>
2021-05-30 23:45:27 -0400

    first commit

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChartsModule } from 'ng2-charts';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+import { DonaComponent } from './pages/dona/dona.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    BarrasDobleComponent,
+    DonaHttpComponent,
+    GraficaBarraComponent,
+    DonaComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule,
+    ChartsModule
+  ]
+})
+export class GraficasModule { }
